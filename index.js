c3296ed698db5e5a037b95b7791dccf65f26c41b
Khaled Alam <khaledalam.net@gmail.com>
2020-09-04 06:53:39 +0200

    1.0.9

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 // Author: Khaled Alam (khaledalam.net@gmail.com)
 
-const instagrammerUsername = 'instagram';
-const instagrammerPostId = 'CCq1D_cMYMF';
-
-const getIGUrl = (username = instagrammerUsername) => 'https://www.instagram.com/' + username + '/?__a=1&is_video=true';
-const getPOSTUrl = (post_id = instagrammerPostId) => 'https://www.instagram.com/p/' + post_id + '/?__a=1';
-
+const username = 'instagram';
+const postId = 'CCq1D_cMYMF';
+const boolToString = (boolValue) => boolValue ? 'true' : 'false';
+const getIGUrl = (username = username) => 'https://www.instagram.com/' + username + '/?__a=1&is_video=true';
+const getPOSTUrl = (postId = postId) => 'https://www.instagram.com/p/' + postId + '/?__a=1';
+const errorMessage = 'Cannot detect option';
 
 /**
  * Scrape instagram profile media and more useful information.
@@ -16,19 +16,17 @@ const getPOSTUrl = (post_id = instagrammerPostId) => 'https://www.instagram.com/
  *
  * @param {string} username      Instagram profile username.
  */
-const profile = async(username = instagrammerUsername) => {
+const profile = async(username = username) => {
 
     try {
         const response = await axios.get(getIGUrl(username));
 
-        let data = response.data.graphql.user.edge_owner_to_timeline_media.edges;
+        return response.data.graphql.user.edge_owner_to_timeline_media.edges || errorMessage;
 
-        return data;
     } catch (error) {
         console.error('instagrammer_profile_media', error);
         return error;
     }
-
 }
 
 /**
@@ -38,16 +36,13 @@ const profile = async(username = instagrammerUsername) => {
  *
  * @param {string} postId      Instagram post id.
  */
-const postComments = async(postId = instagrammerPostId) => {
+const postComments = async(postId = postId) => {
 
     try {
         const response = await axios.get(getPOSTUrl(postId));
 
-        let data = response.data.graphql.shortcode_media.edge_media_to_parent_comment.edges;
+        return response.data.graphql.shortcode_media.edge_media_to_parent_comment.edges || errorMessage;
 
-        console.log(data)
-
-        return data;
     } catch (error) {
         console.error('instagrammer_post_comments', error);
         return error;
@@ -61,14 +56,13 @@ const postComments = async(postId = instagrammerPostId) => {
  *
  * @param {string} postId      Instagram post id.
  */
-const postDimensions = async(postId = instagrammerPostId) => {
+const postDimensions = async(postId = postId) => {
 
     try {
         const response = await axios.get(getPOSTUrl(postId));
 
-        let data = response.data.graphql.shortcode_media.dimensions || 'No Dimensions';
+        return response.data.graphql.shortcode_media.dimensions || errorMessage;
 
-        return data;
     } catch (error) {
         console.error('instagrammer_post_dimensions', error);
         return error;
@@ -82,14 +76,13 @@ const postDimensions = async(postId = instagrammerPostId) => {
  *
  * @param {string} postId      Instagram post id.
  */
-const postCaption = async(postId = instagrammerPostId) => {
+const postCaption = async(postId = postId) => {
 
     try {
         const response = await axios.get(getPOSTUrl(postId));
 
-        let data = response.data.graphql.shortcode_media.accessibility_caption || 'No Caption';
+        return response.data.graphql.shortcode_media.accessibility_caption || errorMessage;
 
-        return data;
     } catch (error) {
         console.error('instagrammer_post_caption', error);
         return error;
@@ -97,20 +90,19 @@ const postCaption = async(postId = instagrammerPostId) => {
 }
 
 /**
- * Scrape instagram post is video.
- * @since      1.0.3
+ * Scrape instagram post and check if post is a video or not.
+ * @since      1.0.7
  * @access     public
  *
  * @param {string} postId      Instagram post id.
  */
-const postIsVideo = async(postId = instagrammerPostId) => {
+const isPostVideo = async(postId = postId) => {
 
     try {
         const response = await axios.get(getPOSTUrl(postId));
 
-        let data = response.data.graphql.shortcode_media.accessibility_caption || 'Cannot detect isVideo';
+        return boolToString(response.data.graphql.shortcode_media.is_video);
 
-        return data;
     } catch (error) {
         console.error('instagrammer_post_is_video', error);
         return error;
@@ -118,10 +110,77 @@ const postIsVideo = async(postId = instagrammerPostId) => {
 }
 
 
+/**
+ * Scrape instagram post and check if caption edited or not.
+ * @since      1.0.9
+ * @access     public
+ *
+ * @param {string} postId      Instagram post id.
+ */
+const isCaptionEdited = async(postId = postId) => {
+
+    try {
+        const response = await axios.get(getPOSTUrl(postId));
+
+        return boolToString(response.data.graphql.shortcode_media.caption_is_edited);
+
+    } catch (error) {
+        console.error('instagrammer_post_is_video', error);
+        return error;
+    }
+}
+
+
+/**
+ * Scrape instagram post and check if comments disabled or not.
+ * @since      1.0.9
+ * @access     public
+ *
+ * @param {string} postId      Instagram post id.
+ */
+const isCommentsDisabled = async(postId = postId) => {
+
+    try {
+        const response = await axios.get(getPOSTUrl(postId));
+
+        return boolToString(response.data.graphql.shortcode_media.comments_disabled);
+
+    } catch (error) {
+        console.error('instagrammer_post_is_video', error);
+        return error;
+    }
+}
+
+/**
+ * Scrape instagram token post timestamp.
+ * @since      1.0.9
+ * @access     public
+ *
+ * @param {string} postId      Instagram post id.
+ */
+const takenTimestamp = async(postId = postId) => {
+
+    try {
+        const response = await axios.get(getPOSTUrl(postId));
+
+        return response.data.graphql.shortcode_media.taken_at_timestamp || errorMessage;
+
+    } catch (error) {
+        console.error('instagrammer_post_is_video', error);
+        return error;
+    }
+}
+
+
+
+
 module.exports = {
     profile,
     postComments,
     postDimensions,
     postCaption,
-    postIsVideo
+    isPostVideo,
+    isCaptionEdited,
+    isCommentsDisabled,
+    takenTimestamp,
 };
